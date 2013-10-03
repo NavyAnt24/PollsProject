9dea385f15f4bf380598b0cf42751ec8430b2826
App Academy Student <student@appacademy.io>
2013-10-03 11:18:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollsProject::Application.config.secret_token = '858378c4ee027a69fa42f1a374402c378214b90c4ed78b449cb81b2b0e9ca1e621eb47c5b1a47c14fbde03a0cfa933c20c748fdf16584ace377575a7282174cd'
